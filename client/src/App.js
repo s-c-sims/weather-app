4a78517d1baf7747db2a5318fe31981473080f70
s-c-sims <sydney.csims@gmail.com>
2021-09-01 15:37:22 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
+function App() 
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container p-3 text-center align-items-center justify-content-center'>
+
+        <h1>Hello world</h1>
+
+
+      </div>
     </div>
   );
 }
