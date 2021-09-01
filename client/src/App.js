9341c4a63b32fa461838d7234a961220bbcc4822
s-c-sims <sydney.csims@gmail.com>
2021-09-01 19:50:43 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'bootswatch/dist/lux/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { fetchOptions, connect, getRows} from './util/connect';
 import { inputDefault, inputBlank, inputNotFound } from './util/validation';
+import { formatDate} from './util/format';
 import Header from './components/Header';
 
 function App() {
@@ -12,10 +13,92 @@ function App() {
 
   const [ location, setLocation ] = useState('');
 
+  const [ msg, setMsg ] = useState('');
   const [ form, setForm ] = useState(inputDefault);
+
+  const [ data, setData ] = useState
+  ({
+    day: [],
+    date: [],
+    forecast: [],
+    low: [],
+    high: [],
+  });
 
 
   let container = 'container d-flex text-center align-items-center justify-content-center';
+
+  const formatColHead = (arr, day, date) =>
+  { 
+    let colHead =  
+    <th> 
+      <h5>{day}</h5>
+      <p>{date}</p>
+    </th>
+
+    arr.push(colHead);
+  };
+
+  const formatData = (arr, item) =>
+  {
+    let element = <td>{item}</td>
+    arr.push(element)
+  };
+
+  const connectBackend = () =>
+  {
+    const options = fetchOptions(JSON.stringify(input));
+
+    const conn = connect('/post', options);
+
+    return conn;
+  };
+
+
+  const handleBackend= (conn) =>
+  {
+    conn.then(result => 
+      {
+        const rows = getRows(result);
+
+        if(!rows) setForm(inputNotFound)
+
+        else
+        { 
+          setLocation(rows.location.name);
+
+          let days = [];
+          let dates = [];
+          let forecast = [];
+          let lows = [];
+          let highs = [];
+
+          rows.forecast.forEach(day =>
+          {
+          
+            formatData(dates, formatDate(day.date));
+            formatData(forecast, day.skytextday);
+            formatData(lows, day.low);
+            formatData(highs, day.high);
+
+          
+
+          });
+
+          setData
+          ({
+            day: days,
+            date: dates,
+            forecast: forecast,
+            low: lows,
+            high: highs
+          });
+
+
+        };
+
+      });
+  };
 
   const handleSubmit = (e) =>
   {
@@ -23,21 +106,10 @@ function App() {
     if(!input.search) setForm(inputBlank)
     else 
     { 
-      const options = fetchOptions(JSON.stringify(input));
 
-      const conn = connect('/post', options);
-    
-      conn.then(result => 
-      {
-        const rows = getRows(result);
-
-        if(!rows) setForm(inputNotFound)
-        else
-        { 
-          setLocation(rows.location.name);
-        };
-
-      });
+      const conn = connectBackend();
+      handleBackend(conn);
+      
     };
   };
   return (
@@ -47,7 +119,8 @@ function App() {
       <Header></Header>
 
       <div className={container}>
-     
+ 
+    
         <form noValidate onSubmit={handleSubmit}>
 
           <label className="col-form-label mt-4" htmlFor="input">
@@ -85,15 +158,34 @@ function App() {
 
       <div className={container}>
         <div className='row'>
-          <table>
+          <table className='table'>
             <thead>
-
+              <th scope='col'></th>
+              
+              {data.day}
+              
             </thead>
+            
+            <tr>
+              <th scope='row'></th>
+              {data.forecast}
+            </tr>
+            <tr>
+            <th scope='row'></th>
+              {data.low}
+            </tr>
+            <tr>
+            <th scope='row'></th>
+              {data.high}
+            </tr>
+            
+            
+          
 
 
           </table>
-        </div>
 
+      </div>
       </div>
 
 
@@ -104,3 +196,4 @@ function App() {
 }
 
 export default App;
+
---
+++ client/src/util/format.js
@@ -0,0 +1,24 @@
+
+const monthNames = 
+[
+    "Jan", "Feb", "March", 
+    "April", "May", "June", "July", 
+    "Aug", "Sept", "Oct", 
+    "Nov", "Dec"                 
+];
+
+const formatDate = (date) =>
+{
+    const d = new Date(date);
+
+    const month = d.getUTCMonth();
+
+    const day = d.getUTCDate();
+
+    const dateFormat = monthNames[month] + ' ' + day;
+
+    return dateFormat;
+};
+
+
+module.exports={formatDate};
