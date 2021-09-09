5ae5516e758474df2d26f61d6258c306d3e19b60
s-c-sims <sydney.csims@gmail.com>
2021-09-09 11:07:10 -0400

    Create ColHead.js

---
+++ client/src/components/ColHead.js
@@ -0,0 +1,16 @@
+import { formatDate } from "../util/format";
+const ColHead = (props) =>
+{
+    const date = formatDate(props.date)
+    return (
+        <th> 
+        <h5>{props.day}</h5>
+        <p>{date}</p>
+        </th>
+
+    )
+
+
+};
+
+export default ColHead;
